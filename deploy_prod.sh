542619f815ac1dab1c58126bf5e20d61d712d663
Joe Cheng <joe@joecheng.com>
2010-10-10 20:19:16 -0700

    Update deployment script to point to new server

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 staticmatic build website
-rsync -rPt --delete --exclude=.idea --exclude=.DS_Store website/ overlake@overlake5k.org:/home/overlake/overlake5k.org/
+rsync -rPt --delete --exclude=.idea --exclude=.DS_Store website/ occsoft@overlake5k.org:/home/occsoft/overlake5k.org/
