f82ba8f927320c8791f5283e912774c8401302d4
Joe Cheng <joe@rstudio.org>
2012-03-08 00:48:55 -0800

    Changes for 2012

---
+++ website/config/site.rb
@@ -0,0 +1,2 @@
+::RootDir = Dir.pwd()
+::RegistrationLink = 'http://www.active.com/event_detail.cfm?event_id=2016642'
--- website/src/configuration.rb
+++
@@ -1,2 +0,0 @@
-RootDir = Dir.pwd()
-RegistrationLink = 'http://www.active.com/register/index.cfm?event_id=1884109'
