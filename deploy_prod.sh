a73b7f0514dab5d9bba9baf7281dff9c5229790c
Joe Cheng <joe@rstudio.org>
2012-06-15 21:24:09 -0700

    Use bundle when building

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-staticmatic build website
+bundle exec staticmatic build website
 rsync -rPt --delete --exclude=.idea --exclude=.DS_Store website/ occsoft@overlake5k.org:/home/occsoft/overlake5k.org/
